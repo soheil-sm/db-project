5f6b144651d6e9a9ca6b91c87cff084307c1b746
soheil mahmoodi <soheilmahmoodisr47@gmail.com>
2024-01-06 17:53:39 +0330

    initial commit

---
+++ src/main/java/ir/ac/hut/dbproject/DbProjectApplication.java
@@ -0,0 +1,13 @@
+package ir.ac.hut.dbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/ac/hut/dbproject/DbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.ac.hut.dbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
